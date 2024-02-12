b1b98403c38407ef6bf4b0de0488116987b8cd92
rutwik <rutuk72@gmail.com>
2024-02-12 22:38:26 +0530

    committwo

--- src/test/java/com/Testproj/base/browser.java
+++ src/test/java/com/Testproj/base/browser.java
@@ -14,12 +14,12 @@ public class browser {
 
     @BeforeTest
     public void setup() throws InterruptedException {
+        //chromedriver path
         System.setProperty("webdriver.chrome.driver", "D:\\javapro\\AmezontestProj\\src\\test\\resources\\driver2\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://www.amazon.in/");
-
 
 
 
